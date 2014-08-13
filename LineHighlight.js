c5a37d84f1ae145faaec2f1aa7edd91cc656b843
Erik Zetterström <erik.ivar.zetterstrom@gmail.com>
2014-08-13 22:39:37 +0200

    Update LineHighlight.js
    Trimmed spaces from the front of the backward selection and the end of the forward selection.
    Trimmed leading and ending tags added by cloneContents().
    Adds more <span> tags as needed due to closing tags (</sometag>) at the start of the clicked line.

--- LineHighlight.js
+++ LineHighlight.js
@@ -40,7 +40,45 @@ var getHTMLOfSelection = function() {
       else {
         return '';
       }
-    }
+ }
+    
+ var trimTags = function(strHTML) {
+     //cloneContents closes any open tags check if the string ends with tags
+     //even if there are original ending tags remove these anyway as it is only the content we want to change
+     while(strHTML[strHTML.length-1]=='>') {
+     		var startIndexOfTag = strHTML.length-1;
+			startIndexOfTag=strHTML.lastIndexOf('<');
+			strHTML = strHTML.slice(0,startIndexOfTag);
+     }
+     
+     //cloneContents closes any open tags check if the string begins with tags
+     //even if there are original opening tags remove these anyway as it is only the content we want to change
+     while(strHTML[0]=='<') {
+     		var endIndexOfTag = 0;
+			endIndexOfTag=strHTML.indexOf('>');
+			strHTML = strHTML.slice(endIndexOfTag+1,strHTML.length);
+     }
+     return strHTML;	
+}
+ 
+ var addSpanTags = function(lineStr) {
+	
+     //find the first closing tag in the string that stops the <span style=...>
+     var firstClosingTagIndex = null;
+     firstClosingTagIndex=lineStr.indexOf('<');
+     var endIndexOfTag = lineStr.indexOf('>');
+     	
+     //Make sure it is a closing tag
+     if(firstClosingTagIndex!=null && lineStr[firstClosingTagIndex+1]=='/') {
+     	var firstPart = lineStr.slice(0,firstClosingTagIndex);
+		var tag = lineStr.slice(firstClosingTagIndex,endIndexOfTag+1);
+     	var secondPart = lineStr.slice(endIndexOfTag+1,lineStr.length);
+     	return (firstPart+"</span>"+tag+"<span style=\"background-color: yellow;\">"+addSpanTags(secondPart));
+     }
+     else {
+     	return lineStr;
+     }
+}
  
  var myClickHandler = function(event) {
      var backwardSelection;
@@ -50,19 +88,22 @@ var getHTMLOfSelection = function() {
      var selectionPoint = window.getSelection();
      selectionPoint.modify('extend','backward','lineboundary');        
      backwardSelection = getHTMLOfSelection();
+     //trim leading ' '
+     while(backwardSelection[0]==' ') {
+     	backwardSelection = backwardSelection.slice(1,backwardSelection.length);
+     }
+     backwardSelection = trimTags(backwardSelection);
 
      selectionPoint.modify('extend','forward','lineboundary');
      forwardSelection = getHTMLOfSelection();
+     //trim ending ' '
+     while(forwardSelection[forwardSelection.length-1]==' ') {
+     	forwardSelection = forwardSelection.slice(0,forwardSelection.length-1);
+     }
+     forwardSelection = trimTags(forwardSelection);
      
      lineHTML = backwardSelection+forwardSelection;
      
-     //cloneContents closes any open tags check if the string ends with a tag
-     if(lineHTML[lineHTML.length-1]=='>') {
-  	var startIndexOfTag = lineHTML.length-1;
-	startIndexOfTag=lineHTML.lastIndexOf('<');
-	lineHTML = lineHTML.slice(0,startIndexOfTag);
-     }
-   	 
      selectionPoint.modify('move','forward','character');
    	
      //Restore the previously clicked line	
@@ -71,18 +112,25 @@ var getHTMLOfSelection = function() {
      }
      previousTarget = event.target;
    	 
-     //Change the selected line
      var org = event.target.innerHTML;
-     originalHTML = org; //Save for restoring
+     //Save for restoring
+     originalHTML = org; 
    	 
      //Change the current line
      var res = null;
+     var lineCopy = lineHTML;
+     
+     //Add <span> elements as needed
+     lineCopy = addSpanTags(lineCopy);
+     
+     //Change the selected line 
      if(org.length>lineHTML.length) {
-     	res = org.replace(lineHTML,"<span style=\"background-color: yellow;\">"+lineHTML+"</span>");
+     	res = org.replace(lineHTML,"<span style=\"background-color: yellow;\">"+lineCopy+"</span>");
      }
      else {
      	res = "<span style=\"background-color: yellow;\">"+org+"</span>";
-     }       
+     } 
+     
      event.target.innerHTML = res;
 }
 
