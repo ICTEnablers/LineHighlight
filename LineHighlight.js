f5d6235a11dee064338c6b54c9e904bbb6a11ff3
Erik Zetterström <erik.ivar.zetterstrom@gmail.com>
2014-08-14 02:08:30 +0200

    Update LineHighlight.js
    Fixes for Firefox.

--- LineHighlight.js
+++ LineHighlight.js
@@ -46,24 +46,48 @@ var getHTMLOfSelection = function() {
      //cloneContents closes any open tags check if the string ends with tags
      //even if there are original ending tags remove these anyway as it is only the content we want to change
      while(strHTML[strHTML.length-1]=='>') {
-     		var startIndexOfTag = strHTML.length-1;
-			startIndexOfTag=strHTML.lastIndexOf('<');
-			strHTML = strHTML.slice(0,startIndexOfTag);
+	var startIndexOfTag = strHTML.length-1;
+	startIndexOfTag=strHTML.lastIndexOf('<');
+	strHTML = strHTML.slice(0,startIndexOfTag);
+			
+	//Trim any accidental ' ' in between tags
+	while(strHTML[strHTML.length-1]==' ') {
+     		strHTML = strHTML.slice(0,strHTML.length-1);
+	}	
      }
      
      //cloneContents closes any open tags check if the string begins with tags
      //even if there are original opening tags remove these anyway as it is only the content we want to change
      while(strHTML[0]=='<') {
-     		var endIndexOfTag = 0;
-			endIndexOfTag=strHTML.indexOf('>');
-			strHTML = strHTML.slice(endIndexOfTag+1,strHTML.length);
+     	var endIndexOfTag = 0;
+	endIndexOfTag=strHTML.indexOf('>');
+			
+	//Fix for Firefox
+	var tag = strHTML.slice(0,endIndexOfTag);
+	var tagContainsAccessibility = -1;
+	tagContainsAccessibility = tag.search("accessibility");
+	if(tagContainsAccessibility!=-1) {
+		strHTML = strHTML.slice(endIndexOfTag+1,strHTML.length);
+		endIndexOfTag=strHTML.indexOf('>');	
+	}
+	strHTML = strHTML.slice(endIndexOfTag+1,strHTML.length);
+			
+	//Ugly fix for Firefox
+	if(strHTML[0]=='^') {
+		strHTML = strHTML.slice(1,strHTML.length);
+	}	
+			
+	//Trim any accidental ' ' in between tags
+	while(strHTML[0]==' ') {
+        	strHTML = strHTML.slice(1,strHTML.length);
+   	}
      }
-     return strHTML;	
+     return strHTML;
 }
  
- var addSpanTags = function(lineStr) {
+var addSpanTags = function(lineStr) {
 	
-     //find the first closing tag in the string that stops the <span style=...>
+    //find the first closing tag in the string that stops the <span style=...>
      var firstClosingTagIndex = null;
      firstClosingTagIndex=lineStr.indexOf('<');
      var endIndexOfTag = lineStr.indexOf('>');
@@ -71,7 +95,7 @@ var getHTMLOfSelection = function() {
      //Make sure it is a closing tag
      if(firstClosingTagIndex!=null && lineStr[firstClosingTagIndex+1]=='/') {
      	var firstPart = lineStr.slice(0,firstClosingTagIndex);
-		var tag = lineStr.slice(firstClosingTagIndex,endIndexOfTag+1);
+	var tag = lineStr.slice(firstClosingTagIndex,endIndexOfTag+1);
      	var secondPart = lineStr.slice(endIndexOfTag+1,lineStr.length);
      	return (firstPart+"</span>"+tag+"<span style=\"background-color: yellow;\">"+addSpanTags(secondPart));
      }
@@ -81,60 +105,60 @@ var getHTMLOfSelection = function() {
 }
  
  var myClickHandler = function(event) {
-     var backwardSelection;
-     var forwardSelection;
-     var lineHTML;
-
-     var selectionPoint = window.getSelection();
-     selectionPoint.modify('extend','backward','lineboundary');        
-     backwardSelection = getHTMLOfSelection();
-     //trim leading ' '
-     while(backwardSelection[0]==' ') {
+      var backwardSelection = null;
+      var forwardSelection = null;
+      var lineHTML = null;
+   	  
+      var selectionPoint = window.getSelection();
+      selectionPoint.modify('extend','backward','lineboundary');        
+      backwardSelection = getHTMLOfSelection();
+         
+      //trim leading ' '
+      while(backwardSelection[0]==' ') {
      	backwardSelection = backwardSelection.slice(1,backwardSelection.length);
-     }
-     backwardSelection = trimTags(backwardSelection);
+      }
+      backwardSelection = trimTags(backwardSelection);
 
-     selectionPoint.modify('extend','forward','lineboundary');
-     forwardSelection = getHTMLOfSelection();
-     //trim ending ' '
-     while(forwardSelection[forwardSelection.length-1]==' ') {
+      selectionPoint.modify('extend','forward','lineboundary');
+      forwardSelection = getHTMLOfSelection();
+      //trim ending ' '
+      while(forwardSelection[forwardSelection.length-1]==' ') {
      	forwardSelection = forwardSelection.slice(0,forwardSelection.length-1);
-     }
-     forwardSelection = trimTags(forwardSelection);
+      }
+      forwardSelection = trimTags(forwardSelection);
      
-     lineHTML = backwardSelection+forwardSelection;
+      lineHTML = backwardSelection+forwardSelection;
      
-     selectionPoint.modify('move','forward','character');
+      selectionPoint.modify('move','forward','character');
    	
-     //Restore the previously clicked line	
-     if(previousTarget != null) {
+      //Restore the previously clicked line	
+      if(previousTarget != null) {
    	previousTarget.innerHTML = originalHTML;
-     }
-     previousTarget = event.target;
+      }
+      previousTarget = event.target;
    	 
-     var org = event.target.innerHTML;
-     //Save for restoring
-     originalHTML = org; 
-   	 
-     //Change the current line
+      var org = event.target.innerHTML;
+      //Save for restoring
+      originalHTML = org;
+   	
      var res = null;
      var lineCopy = lineHTML;
      
      //Add <span> elements as needed
      lineCopy = addSpanTags(lineCopy);
-     
-     //Change the selected line 
+
+     //Change the selected line      
      if(org.length>lineHTML.length) {
      	res = org.replace(lineHTML,"<span style=\"background-color: yellow;\">"+lineCopy+"</span>");
      }
      else {
      	res = "<span style=\"background-color: yellow;\">"+org+"</span>";
      } 
-     
+      
      event.target.innerHTML = res;
 }
 
 
 init();
-
+	
 })(window);
